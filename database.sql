de4be27dd6e8bd5aa1c18d8901e446939ce6f365
Abdul  Ismail <abdulismail@Abduls-MacBook-Air.local>
2019-01-07 09:28:27 -0600

    database set up

--- database.sql
+++ database.sql
@@ -1,5 +1,36 @@
-CREATE TABLE "person" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+
+CREATE TABLE "Users" (
+	"id" serial NOT NULL,
+	"username" Varchar(40) NOT NULL,
+	"password" Varchar(60) NOT NULL,
+	"Admin" BOOLEAN NOT NULL,
+	CONSTRAINT Users_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "events" (
+	"name" varchar(250) NOT NULL,
+	"date" varchar(250) NOT NULL,
+	"time" TIME NOT NULL,
+	"id" serial NOT NULL,
+	"description" varchar(10000) NOT NULL,
+	"Location" varchar(10000) NOT NULL,
+	CONSTRAINT events_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "Going_to_event" (
+	"interested" varchar(60) NOT NULL,
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"event_id" varchar(60) NOT NULL,
+	CONSTRAINT Going_to_event_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
